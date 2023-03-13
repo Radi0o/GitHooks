200db4826c8e6b80ec57622566028cb649d0e445
Zffu <103074097+Radi0o@users.noreply.github.com>
2023-03-13 17:24:38 +0100

    Update [hook].js

--- pages/api/hook/[hook].js
+++ pages/api/hook/[hook].js
@@ -4,5 +4,8 @@ export default function handler(req, res) {
   const ids = req.query.hook.split(";")
   const serverId = ids[0];
   const hookId = ids[1];
+  if(serverId == undefined || hookId == undefined) {
+    res.status(200).json({ error: 'Webhook ID is not parsing correctly' })
+  }
   res.status(200).json({ name: 'John Doe' })
 }
