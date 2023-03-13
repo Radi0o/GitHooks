f1b985a660cb3402428db0edae5cdb668204b20c
Zffu <103074097+Radi0o@users.noreply.github.com>
2023-03-13 17:20:45 +0100

    Update and rename pages/api/hook.js to pages/api/hook/[hook].js

--- pages/api/hook/[hook].js
+++ pages/api/hook/[hook].js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
+  const ids = req.query.hook.split(";")
+  const serverId = ids[0];
+  const hookId = ids[1];
   res.status(200).json({ name: 'John Doe' })
 }
